6652d308b2ef3768ec8c14fb0a6e40b4cea62bc7
pariwatxdady-dev <pariwatxdady@gmail.com>
2026-03-06 16:39:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,17 +4,23 @@ const topics = [
 title:"คอมพิวเตอร์",
 content:`
 
-<img class="topic-img" src="https://images.unsplash.com/photo-1518770660439-4636190af475">
+<img class="topic-img" src="images/computer.jpg">
 
 <h3>คอมพิวเตอร์คืออะไร</h3>
-<p>คอมพิวเตอร์คืออุปกรณ์ที่ใช้รับข้อมูล ประมวลผล และแสดงผล</p>
+<p>คอมพิวเตอร์คืออุปกรณ์ที่ใช้รับข้อมูลและประมวลผล</p>
 
 <ul>
-<li>CPU หน่วยประมวลผล</li>
-<li>RAM หน่วยความจำ</li>
-<li>Storage เก็บข้อมูล</li>
-<li>Input / Output</li>
+<li>CPU</li>
+<li>RAM</li>
+<li>Storage</li>
 </ul>
+
+<h3>ตัวอย่าง</h3>
+
+<div class="example-box">
+<img src="examples/computer-example.jpg">
+<p>ตัวอย่างคอมพิวเตอร์ตั้งโต๊ะ</p>
+</div>
 
 `
 },
@@ -23,7 +29,7 @@ content:`
 title:"อินเทอร์เน็ต",
 content:`
 
-<img class="topic-img" src="https://images.unsplash.com/photo-1509395176047-4a66953fd231">
+<img class="topic-img" src="images/internet.jpg">
 
 <h3>อินเทอร์เน็ต</h3>
 <p>เครือข่ายที่เชื่อมต่อคอมพิวเตอร์ทั่วโลก</p>
@@ -32,26 +38,14 @@ content:`
 <li>เว็บไซต์</li>
 <li>อีเมล</li>
 <li>โซเชียลมีเดีย</li>
-<li>Cloud</li>
 </ul>
 
-`
-},
+<h3>ตัวอย่าง</h3>
 
-{
-title:"AI",
-content:`
-
-<img class="topic-img" src="https://images.unsplash.com/photo-1677442136019-21780ecad995">
-
-<h3>Artificial Intelligence</h3>
-<p>เทคโนโลยีที่ทำให้คอมพิวเตอร์สามารถเรียนรู้และตัดสินใจได้</p>
-
-<ul>
-<li>Chatbot</li>
-<li>Face Recognition</li>
-<li>Self Driving Car</li>
-</ul>
+<div class="example-box">
+<img src="examples/internet-example.jpg">
+<p>ตัวอย่างการใช้งานอินเทอร์เน็ต</p>
+</div>
 
 `
 }
@@ -60,10 +54,10 @@ content:`
 
 function showContent(index){
 
-document.getElementById("contentBox").innerHTML = `
+document.getElementById("contentBox").innerHTML=`
 
 <h2>${topics[index].title}</h2>
 ${topics[index].content}
-`;
+`
 
 }
