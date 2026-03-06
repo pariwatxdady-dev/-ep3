31a9ebe0d104c4e7f294955d584141da01cdeb69
pariwatxdady-dev <pariwatxdady@gmail.com>
2026-03-06 16:44:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,24 +4,15 @@ const topics = [
 title:"คอมพิวเตอร์",
 content:`
 
-<img class="topic-img" src="images/computer.jpg">
-
 <h3>คอมพิวเตอร์คืออะไร</h3>
-<p>คอมพิวเตอร์คืออุปกรณ์ที่ใช้รับข้อมูลและประมวลผล</p>
+<p>คอมพิวเตอร์คืออุปกรณ์ที่ใช้ประมวลผลข้อมูลตามคำสั่ง</p>
 
 <ul>
 <li>CPU</li>
 <li>RAM</li>
 <li>Storage</li>
+<li>Input / Output</li>
 </ul>
-
-<h3>ตัวอย่าง</h3>
-
-<div class="example-box">
-<img src="examples/computer-example.jpg">
-<p>ตัวอย่างคอมพิวเตอร์ตั้งโต๊ะ</p>
-</div>
-
 `
 },
 
@@ -29,35 +20,87 @@ content:`
 title:"อินเทอร์เน็ต",
 content:`
 
-<img class="topic-img" src="images/internet.jpg">
-
 <h3>อินเทอร์เน็ต</h3>
 <p>เครือข่ายที่เชื่อมต่อคอมพิวเตอร์ทั่วโลก</p>
 
 <ul>
 <li>เว็บไซต์</li>
 <li>อีเมล</li>
-<li>โซเชียลมีเดีย</li>
+<li>Cloud</li>
+<li>Video call</li>
 </ul>
+`
+},
 
-<h3>ตัวอย่าง</h3>
+{
+title:"Cyber Security",
+content:`
 
-<div class="example-box">
-<img src="examples/internet-example.jpg">
-<p>ตัวอย่างการใช้งานอินเทอร์เน็ต</p>
-</div>
+<h3>ความปลอดภัยไซเบอร์</h3>
+<p>การป้องกันข้อมูลจากการโจมตีทางอินเทอร์เน็ต</p>
 
+<ul>
+<li>Virus</li>
+<li>Malware</li>
+<li>Phishing</li>
+<li>Ransomware</li>
+</ul>
+`
+},
+
+{
+title:"AI",
+content:`
+
+<h3>Artificial Intelligence</h3>
+<p>เทคโนโลยีที่ทำให้คอมพิวเตอร์เรียนรู้และตัดสินใจได้</p>
+
+<ul>
+<li>Chatbot</li>
+<li>Face recognition</li>
+<li>Self-driving car</li>
+</ul>
+`
+},
+
+{
+title:"Programming",
+content:`
+
+<h3>การเขียนโปรแกรม</h3>
+<p>การเขียนคำสั่งให้คอมพิวเตอร์ทำงาน</p>
+
+<ul>
+<li>HTML</li>
+<li>CSS</li>
+<li>JavaScript</li>
+<li>Python</li>
+</ul>
+`
+},
+
+{
+title:"Social Media",
+content:`
+
+<h3>โซเชียลมีเดีย</h3>
+<p>แพลตฟอร์มสำหรับการสื่อสารและแบ่งปันข้อมูล</p>
+
+<ul>
+<li>Facebook</li>
+<li>Instagram</li>
+<li>TikTok</li>
+<li>YouTube</li>
+</ul>
 `
 }
 
-];
+]
 
 function showContent(index){
 
 document.getElementById("contentBox").innerHTML=`
 
 <h2>${topics[index].title}</h2>
-${topics[index].content}
-`
 
-}
+${topics[index].content}
